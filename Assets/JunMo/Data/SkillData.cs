f8a8ae694307992187e6d91f131d9a079c2caf63
qkshshhd <ssambo33@gmail.com>
2026-02-06 17:03:06 +0900

    이거 창용이가 Enemy를 좀 만들어야지 가능

--- Assets/JunMo/Data/SkillData.cs
+++ Assets/JunMo/Data/SkillData.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "SkillData")]
-public class SkillData : ScriptableObject
+public abstract class SkillData : ScriptableObject
 {
     public string skillName;
     public float cooldown;
-    // public abstract void Execute(GameObject owner);
+    public abstract void Execute(GameObject owner, Vector2 mouseAngle);
 }

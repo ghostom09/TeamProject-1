b759f114085820b827ba9be72fe549def79c68e0
qkshshhd <ssambo33@gmail.com>
2026-02-09 13:27:40 +0900

    +IDamageable(Interface)
    인터페이스 추가함

--- Assets/Scripts_p/Player.cs
+++ Assets/Scripts_p/Player.cs
@@ -1,11 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.TextCore.Text;
 
 public class Player : MonoBehaviour
-{
+{ 
     [SerializeField] private PlayerSkillExecutor playerSkillExecutor;
+    
+    [SerializeField] private CharacterData character;
 
+    private void Start()
+    {
+        Init(character);
+    }
     public void Init(CharacterData data)
     {
         playerSkillExecutor.Init(data.Skills);
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.numpad1Key.wasPressedThisFrame)
+        {
+            playerSkillExecutor.UseSkill(0,Vector2.zero);
+        }else if (Keyboard.current.numpad2Key.wasPressedThisFrame)
+        {
+            playerSkillExecutor.UseSkill(1,Vector2.zero);
+        }
     }
 }

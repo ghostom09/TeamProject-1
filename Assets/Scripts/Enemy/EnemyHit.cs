18bd103db2de18b6f4ab208bbea5619d08d262f9
ccy <casiopea0025@gmail.com>
2026-02-11 18:35:53 +0900

    enemy
    안된거 다시 올림

--- Assets/Scripts/Enemy/EnemyHit.cs
+++ Assets/Scripts/Enemy/EnemyHit.cs
@@ -3,5 +3,17 @@ using UnityEngine;
 
 public class EnemyHit : MonoBehaviour
 {
+    private Enemy enemyStat;
     
+    private float health;
+    
+    private void Awake()
+    {
+        enemyStat = GetComponent<Enemy>();
+    }
+
+    private void OnEnable()
+    {
+        health = enemyStat.stats.health;
+    }
 }

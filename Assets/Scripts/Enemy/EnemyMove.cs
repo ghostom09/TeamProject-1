0d82d67a1e085e242559e24c9d545dcf64229175
ccy <casiopea0025@gmail.com>
2026-02-11 16:34:28 +0900

    enemy
    enemy이동 ai 보강

--- Assets/Scripts/Enemy/EnemyMove.cs
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -1,38 +1,105 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemyMove : MonoBehaviour
 {
     [SerializeField] private GameObject player;
+    [SerializeField] private LayerMask wallLayer;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Transform leftwallCheck;
+    [SerializeField] private Transform rightwallCheck;
     
     private Rigidbody2D rb2d;
-    private BoxCollider2D collider;
+    private Enemy enemyStat;
     private float speed;
+    [SerializeField] private float jumpForce;
+    private int jumpTry = 0;
+    
+    private float groundRadius = 0.2f;
+    private bool isGrounded;
+    private bool isleftWall;
+    private bool isrightWall;
+    private bool isJumping = false;
 
-    private void Start()
+    private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        collider = GetComponent<BoxCollider2D>();
-        speed = GetComponent<Enemy>().stats.speed;
+        enemyStat = GetComponent<Enemy>();
     }
 
-    private void Update()
+    private void OnEnable()
     {
-        horizontalmove();
+        Debug.Log("OnEnable");
+        speed = enemyStat.stats.speed;
+        jumpForce = enemyStat.stats.jumpForce;
     }
+    
+    private void FixedUpdate()
+    {
+        CheckGround();
+        CheckWall();
+        horizontalmove();
+        if (isGrounded)
+        {
+            isJumping = false;
+            if (isleftWall || isrightWall)
+            {
+                verticalmove();
+            }
+            else
+            {
+                if (player.transform.position.y > transform.position.y)
+                {
+                    jumpTry = Random.Range(50, 100);
+                    if(jumpTry == 90)
+                        verticalmove();
+                }
+            }
+        }
+        else if(!isJumping&&!isGrounded&&player.transform.position.y > transform.position.y)
+        {
+            verticalmove();
+        }
+    }
+    
+    private void CheckWall()
+    {
+        isleftWall = Physics2D.OverlapCircle(
+            leftwallCheck.position,
+            groundRadius,
+            wallLayer
+        );
+        isrightWall = Physics2D.OverlapCircle(
+            rightwallCheck.position,
+            groundRadius,
+            wallLayer
+        );
+    }
+
+    private void CheckGround()
+    {
+        isGrounded = Physics2D.OverlapCircle(
+            groundCheck.position,
+            groundRadius,
+            wallLayer
+        );
+    }   
     
     private void verticalmove()
     {
-        
+        rb2d.linearVelocity = new Vector2(rb2d.linearVelocity.x, jumpForce);
+        isJumping = true;
     }
     private void horizontalmove()
     {
         if (player.transform.position.x < transform.position.x)
         {
-            rb2d.linearVelocity = new Vector2(-speed, 0);
+            rb2d.linearVelocity = new Vector2(-speed, rb2d.linearVelocity.y);
         }
-        else if (player.transform.position.x > transform.position.x)
+        else
         {
-            rb2d.linearVelocity = new Vector2(speed, 0);
+            rb2d.linearVelocity = new Vector2(speed, rb2d.linearVelocity.y);
         }
     }
 }
